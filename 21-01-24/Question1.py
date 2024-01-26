24dddc702fdf3d6a3c486c147e488538f1912683
Purushothaman-16122002 <146987545+Purushothaman-16122002@users.noreply.github.com>
2024-01-26 07:28:11 -0800

    Update Question1.py

--- 21-01-24/Question1.py
+++ 21-01-24/Question1.py
@@ -37,3 +37,12 @@ Explanation 1
 n=24
 n>20 and n is even, so it is not weird.
 """
+n = int(input())
+if n % 2 == 1:
+print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+print("Weird")
+else:
+print("Not Weird")
